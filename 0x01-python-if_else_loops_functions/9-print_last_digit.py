513cb623b1b4ed29685033cc4f935292f11a5988
RonnyOtieno20 <ronnyotieno20@gmail.com>
2024-03-14 00:31:50 +0300

    hot fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
+
 def print_last_digit(number):
     """
-        prints the last digit of  number
-        returns the value of the last digit
-        you are not allowed to import any module
-        """
+    prints the last digit of  number
+    returns the value of the last digit
+    you are not allowed to import any module
+    """
     last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
-
