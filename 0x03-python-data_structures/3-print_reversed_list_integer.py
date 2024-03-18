d0b096d8ee31497266b08b222f3ac2b8ed29ae68
RonnyOtieno20 <ronnyotieno20@gmail.com>
2024-03-18 21:30:11 +0300

    python data structures

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
+
 def print_reversed_list_integer(my_list=[]):
     """
-        print all integers of a list, in reverse order
-        format: one integer per line
-        you are not allowed to import any module
-        you can assume that the list only contains integers
-        you are not allowed to cast integers into strings
-        you have to use str.format() to print integers
+    print all integers of a list, in reverse order
+    format: one integer per line
+    you are not allowed to import any module
+    you can assume that the list only contains integers
+    you are not allowed to cast integers into strings
+    you have to use str.format() to print integers
     """
+    if not my_list:
+        return None
     my_list.reverse()
 
     for num in my_list:
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
+
 def multiple_returns(sentence):
     """
-        returns the length of a string and its first character
-        if the sentence is empty, the first character should be equal to None
-        you are not allowed to import any module
+    returns the length of a string and its first character
+    if the sentence is empty, the first character should be equal to None
+    you are not allowed to import any module
     """
-    if sentence == "":
-        return (None)
+    if len(sentence) == 0:
+        return len(sentence), None
     return (len(sentence), sentence[:1])
