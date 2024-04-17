581b13cbcd48503367b5ca4351db4626cb577714
= <=>
2024-04-17 23:13:38 +0300

    python input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """ Module that contains a function that reads from a file """
 
 
@@ -13,6 +12,7 @@ def read_file(filename=""):
         Exception: when the file can be opened
 
     """
-    with open(filename, "r", encoding="UTF-8") as f:
+
+    with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data, end="")
+        print(read_data, end='')
