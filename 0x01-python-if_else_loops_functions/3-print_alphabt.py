a7684cd0f4971b9c4d694654fd720f10020c6d9a
justronny20 <ronnyotieno20@gmail.com>
2024-04-23 14:33:26 +0300

    Delete 0x01-python-if_else_loops_functions directory

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-for i in range(97, 123):
-    if "q" not in chr(i) and "e" not in chr(i):
-        print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-for i in range(1, 99):
-    if (i // 10) < (i % 10):
-        if i == 89:
-            print("89")
-        else:
-            print("{:02}".format(i), end=", ")
