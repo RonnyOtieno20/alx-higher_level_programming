8246010a14d07f8a68de1b0f312467d0a86b5953
RonnyOtieno20 <ronnyotieno20@gmail.com>
2024-04-23 15:37:30 +0300

    print combination of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,6 @@ for num1 in range(0, 10):
         if num1 > num2:
             continue
         elif num1 == 8 and num2 == 9:
-            print("{:d}{:d}".format(num1, num2), end="")
+            print("{:d}{:d}".format(num1, num2))
         else:
             print("{:d}{:d}".format(num1, num2), end=", ")
