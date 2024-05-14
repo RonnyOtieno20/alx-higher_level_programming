7b68d3cda9e1a2485b141c6a58f01e9ba04bb4e1
RonnyOtieno20 <ronnyotieno20@gmail.com>
2024-05-14 12:38:24 +0300

    function that updates dictionary keys and values

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Function that replaces or adds key/value in a dictionary.
+    key argment will always be a string
+    value argument will be any type
+    If a key exists in the dictionary, the value will be replaced
+    If a key doesn't exists, it will be created
+    You are not allowed to import any module
+    Args:
+        a_dictionary: the dictionary in question
+        key: dictionary key
+        value: dictionary value
+    """
+    a_dictionary.update({key: value})
+    return(a_dictionary)
+    
