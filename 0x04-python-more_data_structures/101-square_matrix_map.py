7c62b4955f0f11499c2ab7634d23d20caf267536
RonnyOtieno20 <ronnyotieno20@gmail.com>
2024-05-20 12:16:20 +0300

    function that squares the elements of a matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    return list(map(lambda r: list(map(lambda elem: elem * elem, r)), matrix))
+    return list(map(lambda r: list(map(lambda elem: elem ** 2, r)), matrix))
